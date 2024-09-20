836c45f39c7e277e264fbd71923fbc9a1e0eb0d8
Dangoo2002 <kennydangote893@gmail.com>
2024-09-20 15:01:30 +0300

    hhf

--- server.js
+++ server.js
@@ -374,15 +374,23 @@ app.get('/api/products', async (req, res) => {
 app.get('/phones_laptops', async (req, res) => {
   try {
     const connection = await pool.getConnection();
+    
+
     const sql = `SELECT id, title, description, price, is_new, category_id, image FROM phones_laptops`;
     const [results] = await connection.query(sql);
     connection.release();
 
-  
+
     const productsWithImages = results.map(product => ({
-      ...product,
-      image: product.image ? `data:image/jpeg;base64,${product.image.toString('base64')}` : null
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      is_new: product.is_new, 
+      category_id: product.category_id,
+      image: product.image ? `data:image/jpeg;base64,${product.image.toString('base64')}` : null // Convert BLOB to base64
     }));
+
 
     return res.json({ success: true, products: productsWithImages });
   } catch (error) {
@@ -390,7 +398,6 @@ app.get('/phones_laptops', async (req, res) => {
     return res.status(500).json({ success: false, message: 'Failed to fetch products' });
   }
 });
-
 
 
 app.listen(port, () => {
