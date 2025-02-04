06297bf29cbc2f97eae03a614896c12e0ba99f8b
Dangoo2002 <kennydangote893@gmail.com>
2025-02-04 18:08:37 +0300

    gitignore

--- emailService.js
+++ emailService.js
@@ -7,6 +7,13 @@ const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
 const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
+
+console.log('Env Check:', {
+    CLIENT_ID: !!CLIENT_ID,
+    CLIENT_SECRET: !!CLIENT_SECRET,
+    REFRESH_TOKEN: !!REFRESH_TOKEN
+  });
+
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
